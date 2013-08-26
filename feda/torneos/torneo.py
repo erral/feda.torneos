d25dac8c2700f481769564c24afe17099aa08551
Mikel Larreategi <mlarreategi@codesyntax.com>
2013-08-26 20:52:24 +0200

    add initial implementation and fields

--- feda/torneos/torneo.py
+++ feda/torneos/torneo.py
@@ -21,8 +21,29 @@ from plone.formwidget.contenttree import ObjPathSourceBinder
 from feda.torneos import MessageFactory as _
 
 
+tournament_systems = SimpleVocabulary(
+    [SimpleTerm(value=u'IS', title=_(u'Individual Swiss')),
+     SimpleTerm(value=u'IRR', title=_(u'Individual Round Robin')),
+     SimpleTerm(value=u'IDRR', title=_(u'Individual Double Round Robin')),
+     SimpleTerm(value=u'TS', title=_(u'Team Swiss')),
+     SimpleTerm(value=u'TRR', title=_(u'Team Round Robin')),
+     SimpleTerm(value=u'O', title=_(u'Other (explain in comments)'))
+
+
+     ]
+    )
+
+rating_types = SimpleVocabulary(
+    [SimpleTerm(value=u'standard', title=_(u'Standard chess')),
+     SimpleTerm(value=u'rapid', title=_(u'Rapid chess')),
+     SimpleTerm(value=u'blitz', title=_(u'Blitz chess')),
+
+
+     ]
+    )
+
 # Interface class; used to define content-type schema.
-class ITorneo(form.Schema, IImageScaleTraversable):
+class ITorneo(form.Schema):
     """
     Description of the Example Type
     """
@@ -31,8 +52,87 @@ class ITorneo(form.Schema, IImageScaleTraversable):
     # If you want a model-based interface, edit
     # models/torneo.xml to define the content type
     # and add directives here as necessary.
-    #form.model("models/torneo.xml")
 
+    city = schema.TextLine(
+        title=_(u'City'),
+        required=True,
+        )
+
+    number_of_players = schema.Int(
+        title=_(u'Number of expected players'),
+        required=True,
+        )
+
+    system = schema.Choice(
+        title=_(u'System of play'),
+        vocabulary=tournament_systems,
+        required=True,
+        )
+
+    start_date = schema.Date(
+        title=_(u'Start date'),
+        required=True,
+        )
+
+    end_date = schema.Date(
+        title=_(u'End date'),
+        required=True,
+        )
+
+    chief_arbiter = schema.Int(
+        title=_(u'Chief arbiter'),
+        description=_("Enter arbiter's FIDE ID"),
+        required=True,
+        )
+
+    deputy_arbiters = schema.List(
+        title=_(u'Deputy arbiters'),
+        value_type=schema.Int(title=_(u'Deputy arbiter')),
+        required=False,
+        )
+
+    organizer_id = schema.Int(
+        title=_(u"Organizer's FIDE ID"),
+        description=_("Enter only if the organizer has a FIDE ID"),
+        required=False,
+        )
+
+    organizer_name = schema.TextLine(
+        title=_(u"Organizer's Name"),
+        description=_("Enter only if the organizer does not have a FIDE ID"),
+        required=False,
+        )
+
+    rating_type = schema.Choice(
+        title=_(u'Rating type'),
+        vocabulary=rating_types,
+        required=True,
+        )
+
+    invoice_name = schema.TextLine(
+        title=_(u'Name for the invoice'),
+        required=True
+        )
+
+    invoice_nif_cif = schema.TextLine(
+        title=_(u'NIF/CIF'),
+        required=True
+        )
+
+    invoice_address = schema.TextLine(
+        title=_(u'Address for the invoice'),
+        required=True
+        )
+
+    invoice_city = schema.TextLine(
+        title=_(u'City'),
+        required=True
+        )
+
+    invoice_postal_code = schema.TextLine(
+        title=_(u'Postal code'),
+        required=True
+        )
 
 # Custom content-type class; objects created for this content type will
 # be instances of this class. Use this class to add content-type specific
